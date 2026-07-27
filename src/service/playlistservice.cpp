27252b9237772e02fa689f2cfbe011a0a8373a7a
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2026-07-27 16:34:55 +0330

    Defined the playlist service

---
+++ src/service/playlistservice.cpp
@@ -0,0 +1,91 @@
+#include "songrepository.h"
+#include "playlistservice.h"
+#include "listenerrepository.h"
+#include "playlistrepository.h"
+
+PlaylistService::PlaylistService(ID playlistId, QObject *parent) : AbstractService{parent}
+{
+
+}
+
+Account PlaylistService::owner() const
+{
+    auto result = ListenerRepository::instance().search(mainPlaylist.ownerId);
+    if (result.has_value())
+    {
+        return *result;
+    }
+    else
+    {
+        return {};
+    }
+}
+
+SongList PlaylistService::songs() const
+{
+    return SongRepository::instance().byPlaylist(mainPlaylist.id);
+}
+
+Playlist PlaylistService::playlist() const
+{
+    return mainPlaylist;
+}
+
+bool PlaylistService::isValid() const
+{
+    return mainPlaylist.id != INVALID_ID;
+}
+
+bool PlaylistService::removeSong(ID songId)
+{
+    auto result = SongRepository::instance().search(songId);
+    if (!result)
+    {
+        qWarning() << "Failed to remove the song with ID" << songId << ":" << "Failed to find the song.";
+        return false;
+    }
+    else
+    {
+        auto success = PlaylistRepository::instance().removeSong(mainPlaylist.id, songId);
+        if (success)
+        {
+            emit songRemoved(songId);
+        }
+        return success;
+    }
+}
+
+bool PlaylistService::addSong(ID songId)
+{
+    auto result = SongRepository::instance().search(songId);
+    if (!result)
+    {
+        qWarning() << "Failed to remove the song with ID" << songId << ":" << "Failed to find the song.";
+        return false;
+    }
+    else
+    {
+        auto success = PlaylistRepository::instance().insertSong(mainPlaylist.id, songId);
+        if (success.has_value())
+        {
+            emit songAdded(songId);
+        }
+        return success.has_value();
+    }
+}
+
+bool PlaylistService::updatePlaylist(const PlaylistData &data)
+{
+    auto result = std::optional<ID> { std::nullopt };
+    auto target = Playlist {};
+    target.ownerId = mainPlaylist.ownerId;
+    target.id = mainPlaylist.id;
+    target.data = data;
+    result = PlaylistRepository::instance().save(target);
+    if (result.has_value())
+    {
+        mainPlaylist = target;
+        emit playlistUpdated(mainPlaylist);
+    }
+    return result.has_value();
+}
---
+++ src/service/playlistservice.h
@@ -0,0 +1,31 @@
+#ifndef PLAYLISTSERVICE_H
+#define PLAYLISTSERVICE_H
+
+#include <QObject>
+#include "abstractservice.h"
+
+class PlaylistService : public AbstractService
+{
+public:
+    PlaylistService(ID playlistId, QObject *parent = nullptr);
+
+    Account owner() const;
+    SongList songs() const;
+    Playlist playlist() const;
+    virtual bool isValid() const override;
+
+public slots:
+    bool addSong(ID songId);
+    bool removeSong(ID songId);
+    bool updatePlaylist(const PlaylistData &data);
+
+signals:
+    void songAdded(ID songId);
+    void songRemoved(ID songId);
+    void playlistUpdated(const Playlist &data);
+
+private:
+    Playlist mainPlaylist;
+};
+
+#endif // PLAYLISTSERVICE_H
