6b5fed0b3894a7e92135a199f88828e8805a2f60
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2026-09-12 19:52:28 +0330

    Added an icon to the project

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
+#include <QIcon>
 #include <QApplication>
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/main.ico"));
+
     return app.exec();
 }
